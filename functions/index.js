8c5b8da382901a1ff37139e1109bed08d81b222e
TheWisePigeon <zozozozeph@gmail.com>
2022-02-23 09:26:36 +0000

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -2,13 +2,7 @@ const functions = require("firebase-functions");
 
 const { Configuration, OpenAIApi } = require('openai');
 const configuration = new Configuration({
-    
+    apiKey = process.env.OPENAI_API_KEY
 })
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+
