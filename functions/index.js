a2dd8fa8b6736c92d8b44adc7bd7bb9942971e40
TheWisePigeon <zozozozeph@gmail.com>
2022-02-23 09:37:06 +0000

    dealing with secrets

--- functions/index.js
+++ functions/index.js
@@ -5,4 +5,18 @@ const configuration = new Configuration({
     apiKey = process.env.OPENAI_API_KEY
 })
 
+const openai= new OpenAIApi(configuration);
 
+exports.wisetrader = functions.https.onRequest((request, response)=>{
+
+    const gptCompletion = await openai.createCompletion("text-davinci-001", {
+        prompt: "Jim Cramer recommends selling the following stocks tickers: ",
+        temperature: 0.7,
+        max_tokens: 32,
+        top_p: 1,
+        frequency_penalty: 0,
+        presence_penalty: 0
+    })
+
+    response.send(gptCompletion.data)
+})
